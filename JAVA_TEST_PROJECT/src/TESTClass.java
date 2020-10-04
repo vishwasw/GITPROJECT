08a8917ceb9abe00d604a25c6e11f7f443e4a38f
vishwasw <31597985+vishwasw@users.noreply.github.com>
2020-10-04 16:57:53 -0500

    Update TESTClass.java
    remote changes

--- JAVA_TEST_PROJECT/src/TESTClass.java
+++ JAVA_TEST_PROJECT/src/TESTClass.java
@@ -4,7 +4,7 @@ public class TESTClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World !!!!!More changes");
-       System.out.println("Hello World !!!!!Changed in local changes");
+       System.out.println("Hello World !!!!!Changed in remote changes");
 	}
 
 }
