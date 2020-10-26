f8d3733a184d28c79edd22f3e059d68647c8b4a8
Vishwas <vishwasw@hotmail.com>
2020-10-26 12:37:37 -0500

    added

--- JAVA_TEST_PROJECT/src/TESTClass.java
+++ JAVA_TEST_PROJECT/src/TESTClass.java
@@ -4,11 +4,7 @@ public class TESTClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Changes made by Seema");
-		
-	}
-	
-	public void calculate(int a, int b) {
-		int result = a*b;
+       System.out.println("Changes made by Vishwas");
 	}
 
 }
